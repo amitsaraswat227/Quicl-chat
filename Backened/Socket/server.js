03ba8cf5935b69eaf420a94ff1247bca96df9c7e
Amit Saraswat <saraswatamit227@gmail.com>
2025-07-23 18:08:42 +0530

    Update server.js

--- Backened/Socket/server.js
+++ Backened/Socket/server.js
@@ -7,7 +7,7 @@ const app=express();
 const server = http.createServer(app);
 const io = new Server(server,{
     cors:{
-        origin:"https://chatapp-backend-hjy5.onrender.com",
+        origin:"https://quick-chat-u2jn.onrender.com",
         methods:["GET","POST"],
 
     }
