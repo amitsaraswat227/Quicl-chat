c9ce8f0ad2cacde80ab21970968c44da0c13009f
Amit Saraswat <saraswatamit227@gmail.com>
2025-07-23 18:02:45 +0530

    Update vite.config.js

--- Frontened/vite.config.js
+++ Frontened/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port:3001,
     proxy:{
       "/api": {
-        target:"http://localhost:4002",
+        target:"https://quick-chat-1-vlkj.onrender.com",
         changeOrigin:true,
       }
     }
